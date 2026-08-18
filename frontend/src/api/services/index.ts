665ff14585d567244ec6d9d7902ad8368555736a
Kasyfil <mksyfla@gmail.com>
2026-08-18 11:27:04 +0700

    remove frontend

--- frontend/src/api/services/index.ts
+++
@@ -1,5 +0,0 @@
-export { authService } from './auth';
-export { itemService } from './items';
-export { locationService } from './locations';
-export { partnerService } from './partners';
-export { receiptService } from './receipts';
